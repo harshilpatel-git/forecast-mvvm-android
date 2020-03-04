ec661720eac503844eb602b93bdb8164a1cb7070
harshilpatel <hp18051407@gmail.com>
2020-03-04 20:17:42 +0530

    Initial commit

---
+++ app/src/main/java/com/harshil/weatherforecastmvvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.harshil.weatherforecastmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
